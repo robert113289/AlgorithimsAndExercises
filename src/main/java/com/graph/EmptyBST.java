7c40fdbe4e1ab9bed573007b0d89f9b4fd095cb2
robert113289 <robert113289@gmail.com>
2019-01-07 11:54:03 -0600

    BinarySearchTree

---
+++ src/main/java/com/graph/EmptyBST.java
@@ -0,0 +1,57 @@
+package com.graph;
+
+import sun.invoke.empty.Empty;
+
+public class EmptyBST<D extends Comparable> implements BinarySearchTree<D> {
+	D data = null;
+	EmptyBST left;
+	EmptyBST right;
+
+	public EmptyBST(){
+
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return data == null;
+	}
+
+	@Override
+	public int cardinality() {
+		return 0;
+	}
+
+	@Override
+	public boolean contains(D element) {
+		if(data == null){
+			return false;
+		}
+		if (data == element){
+			return true;
+		} else {
+			if (element.compareTo(data) < 0){
+				return left.contains(element);
+			} else {
+				return right.contains(element);
+			}
+		}
+	}
+
+	@Override
+	public NonEmptyBST<D> add(D elt) {
+		return new NonEmptyBST<D>(elt);
+	}
+	public D getData() {
+		return data;
+	}
+
+	@Override
+	public BinarySearchTree getLeft() {
+		return left;
+	}
+
+	@Override
+	public void setLeft(BinarySearchTree<D> elt) {
+
+	}
+}
